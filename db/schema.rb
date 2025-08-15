8251489b6dad86d9be3f1a92b90373b6e5fae6e1
carlbrizuela <carljasper.brizuela@gmail.com>
2025-08-15 17:37:41 +0800

    Created workouts and exercises models and controllers

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_08_14_095949) do
+ActiveRecord::Schema[7.2].define(version: 2025_08_15_091955) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "exercises", force: :cascade do |t|
+    t.string "exercise_name"
+    t.string "exercise_id"
+    t.integer "sets"
+    t.integer "reps"
+    t.float "weight"
+    t.string "intensity"
+    t.float "distance"
+    t.float "duration"
+    t.bigint "workout_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["workout_id"], name: "index_exercises_on_workout_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -22,4 +37,18 @@ ActiveRecord::Schema[7.2].define(version: 2025_08_14_095949) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "workouts", force: :cascade do |t|
+    t.string "type"
+    t.date "workout_date"
+    t.float "duration"
+    t.float "calories_burned"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_workouts_on_user_id"
+  end
+
+  add_foreign_key "exercises", "workouts"
+  add_foreign_key "workouts", "users"
 end
