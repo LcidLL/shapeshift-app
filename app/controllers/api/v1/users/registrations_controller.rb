270098bbc44ae0f7ac0a0c557cd4f4c96d65abd5
Lcid Lumenario <lumenariolcid@gmail.com>
2025-08-18 16:21:29 +0800

    updated devise controllers and added user_serializer.rb

--- app/controllers/api/v1/users/registrations_controller.rb
+++ app/controllers/api/v1/users/registrations_controller.rb
@@ -1,62 +1,87 @@
-# frozen_string_literal: true
-
 class Api::V1::Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  respond_to :json
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
 
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  # GET /api/v1/profile
+  def show
+    render json: {
+      status: {code: 200, message: 'Profile retrieved successfully.'},
+      data: {user: UserSerializer.new(current_user).serializable_hash[:data][:attributes]}
+    }
+  end
 
-  # POST /resource
-  # def create
-  #   super
-  # end
+  # PATCH /api/v1/profile
+  def update_profile
+    if current_user.update(profile_params)
+      render json: {
+        status: {code: 200, message: 'Profile updated successfully.'},
+        data: {user: UserSerializer.new(current_user).serializable_hash[:data][:attributes]}
+      }
+    else
+      render json: {
+        status: {code: 422, message: 'Profile could not be updated.', errors: current_user.errors.full_messages}
+      }, status: :unprocessable_entity
+    end
+  end
 
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  # POST /api/v1/daily_weight
+  def log_daily_weight
+    if current_user.update(weight: params[:weight])
+      render json: {
+        status: {code: 200, message: 'Weight logged successfully.'},
+        data: {current_weight: current_user.weight, logged_at: Time.current}
+      }
+    else
+      render json: {
+        status: {code: 422, message: 'Weight could not be logged.', errors: current_user.errors.full_messages}
+      }, status: :unprocessable_entity
+    end
+  end
 
-  # PUT /resource
-  # def update
-  #   super
-  # end
+  protected
 
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
+  def respond_with(resource, _opts = {})
+    if resource.persisted?
+      render json: {
+        status: { 
+          code: 200, 
+          message: 'Signed up successfully. Please check your email for confirmation.' 
+        },
+        data: {
+          user: UserSerializer.new(resource).serializable_hash[:data][:attributes]
+        }
+      }
+    else
+      render json: {
+        status: { 
+          code: 422,
+          message: "User couldn't be created successfully.",
+          errors: resource.errors.full_messages
+        }
+      }, status: :unprocessable_entity
+    end
+  end
 
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
+  private
 
-  # protected
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [
+      :first_name, :last_name, :age, :sex, :weight, :height,
+      :daily_calorie_intake, :daily_calories_burned, 
+      :workout_duration, :target_weight
+    ])
+  end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
-
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def profile_params
+    params.require(:user).permit(
+      :first_name, :last_name, :age, :sex, :weight, :height,
+      :daily_calorie_intake, :daily_calories_burned, 
+      :workout_duration, :target_weight
+    )
+  end
 end
